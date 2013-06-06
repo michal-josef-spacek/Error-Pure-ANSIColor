36e4e3b13380224378b389d11962e94cd52d626f
Michal Špaček <tupinek@gmail.com>
2013-06-06 15:10:29 +0200

    Add main module with documentation.

---
+++ ANSIColor.pm
@@ -0,0 +1,45 @@
+package Error::Pure::ANSIColor;
+
+# Pragmas.
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Error::Pure::ANSIColor - Perl module for structured errors with Term::ANSIColor output.
+
+=head1 MODULES
+
+L<Error::Pure::ANSIColor::AllError>,
+L<Error::Pure::ANSIColor::Die>,
+L<Error::Pure::ANSIColor::Error>,
+L<Error::Pure::ANSIColor::ErrorList>,
+L<Error::Pure::ANSIColor::Print>,
+
+=head1 REPOSITORY
+
+L<https://github.com/tupinek/Error-Pure-ANSIColor>
+
+=head1 AUTHOR
+
+Michal Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+BSD license.
+
+=head1 VERSION
+
+0.01
+
+=cut
