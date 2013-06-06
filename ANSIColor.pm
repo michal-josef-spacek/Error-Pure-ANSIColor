0566382539ff2b8adb7931588d1376d70abacfce
Michal Špaček <tupinek@gmail.com>
2013-06-06 15:16:09 +0200

    Improve doc.

--- ANSIColor.pm
+++ ANSIColor.pm
@@ -14,15 +14,56 @@ __END__
 
 =head1 NAME
 
-Error::Pure::ANSIColor - Perl module for structured errors with Term::ANSIColor output.
+Error::Pure::ANSIColor - Error::Pure modules with Term::ANSIColor support.
 
-=head1 MODULES
+=head1 DESCRIPTION
 
+List of modules:
+
+=over 8
+
+=item C<Error::Pure::ANSIColor::AllError>
+
+Print full backtrace with Term::ANSIColor support.
+
+=item C<Error::Pure::ANSIColor::Die>
+
+Die with Term::ANSIColor support.
+
+=item C<Error::Pure::ANSIColor::Error>
+
+Print error on one line with Term::ANSIColor support.
+
+=item C<Error::Pure::ANSIColor::ErrorList>
+
+Print list of errors with ANSIColor support. Each error on one line.
+
+=item C<Error::Pure::ANSIColor::Print>
+
+Print error as simple string with ANSIColor support
+
+=back
+
+=head1 SEE ALSO
+
+L<Error::Pure>,
 L<Error::Pure::ANSIColor::AllError>,
 L<Error::Pure::ANSIColor::Die>,
 L<Error::Pure::ANSIColor::Error>,
 L<Error::Pure::ANSIColor::ErrorList>,
 L<Error::Pure::ANSIColor::Print>,
+L<Error::Pure::AllError>,
+L<Error::Pure::Die>,
+L<Error::Pure::Error>,
+L<Error::Pure::ErrorList>,
+L<Error::Pure::HTTP::AllError>,
+L<Error::Pure::HTTP::Error>,
+L<Error::Pure::HTTP::ErrorList>,
+L<Error::Pure::HTTP::Print>,
+L<Error::Pure::NoDie>,
+L<Error::Pure::Output::Text>,
+L<Error::Pure::Print>,
+L<Error::Pure::Utils>.
 
 =head1 REPOSITORY
 
@@ -35,7 +76,7 @@ Michal Špaček L<mailto:skim@cpan.org>
 L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
-
+.
 BSD license.
 
 =head1 VERSION
